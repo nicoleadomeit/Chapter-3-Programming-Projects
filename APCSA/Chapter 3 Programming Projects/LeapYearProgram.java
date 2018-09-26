8a18b2b3e5535a4f97ac28a78d0c4353cb6db755
nicoleadomeit <43471918+nicoleadomeit@users.noreply.github.com>
2018-09-26 08:06:14 -0400

    Update LeapYearProgram.java

--- APCSA/Chapter 3 Programming Projects/LeapYearProgram.java
+++ APCSA/Chapter 3 Programming Projects/LeapYearProgram.java
@@ -1,3 +1,8 @@
+/*
+Program asks user to enter a year from the Gregorian calendar (after 1582). User enters a year, program tells the person if it is a leap year or not. 
+The program should repeat over and over until the user enters a sentinel value to end the program. Leap years only exist after 1582, so if the year 
+isn’t bigger then that it should re-prompt the user. A year is a leap year if it is divisible by 4, unless it is also divisible by 100 but not 400.
+ */
 import java.util.Scanner;
 public class LeapYearProgram {
     public static void main() {
@@ -6,7 +11,7 @@ public class LeapYearProgram {
         System.out.println ("Please enter a year after the year of 1582.");
         int x = scan.nextInt();
         while (Nicole) {
-            if (((x>1582) && ((x%4==0)) && (!(x%100==0) (x%400==0)) {
+            if ((x>1582) && (x%4==0) && (!(x%100==0)) && (x%400==0)) {
                 System.out.println ("This year is a leap year");
             }
             else if ((x>1582) && (!(x%4==0))){
@@ -23,6 +28,5 @@ public class LeapYearProgram {
         }
     }
 }
-
 
 
