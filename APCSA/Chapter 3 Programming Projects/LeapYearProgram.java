c7a90af1184567a4b710b240cea1fc590bf83b6f
nicoleadomeit <43471918+nicoleadomeit@users.noreply.github.com>
2018-09-27 08:07:18 -0400

    Update LeapYearProgram.java

--- APCSA/Chapter 3 Programming Projects/LeapYearProgram.java
+++ APCSA/Chapter 3 Programming Projects/LeapYearProgram.java
@@ -11,22 +11,23 @@ public class LeapYearProgram {
         System.out.println ("Please enter a year after the year of 1582.");
         int x = scan.nextInt();
         while (Nicole) {
-            if ((x>1582) && (x%4==0) && (!(x%100==0)) && (x%400==0)) {
+            if ((x>1582) && (x%4==0) && (!(x%100==0)) && (!(x%400==0))) {
                 System.out.println ("This year is a leap year");
             }
-            else if ((x>1582) && (!(x%4==0))){
+            else if ((x>1582) && (x%4==0) && (x%100==0) && (!(x%400==0))) {
                 System.out.println ("This year is not a leap year");
             }
             else if (x<=1582) {
-            System.out.println ("This is before Leap Year was recognized");
-        }
-        }
-        System.out.println ("Please enter another year after 1582. Enter 0 to quit the game.");
-        int y = scan.nextInt();
-        if (y == 0) {
-            Nicole = false;
+                System.out.println ("This is before Leap Year was recognized");
+            }
+            else {
+                System.out.println ("This is not a leap year.");
+            }
+            System.out.println ("Please enter another year after 1582. Enter 0 to quit the game.");
+            int y = scan.nextInt();
+            if (y == 0) {
+                Nicole = false;
+            }
         }
     }
 }
-
-
