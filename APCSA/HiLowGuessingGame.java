0da01be1945b808afe1e3568cf914cbecd1931e0
nicoleadomeit <43471918+nicoleadomeit@users.noreply.github.com>
2018-10-02 16:35:54 -0400

    Update HiLowGuessingGame.java

--- APCSA/HiLowGuessingGame.java
+++ APCSA/HiLowGuessingGame.java
@@ -6,23 +6,35 @@ If user gets correct #, ask if they want to play again. */
 import java.util.Scanner;
 public class HiLowGuessingGame {
     public static void main() {
-        int x;
-        int 
-        x = 0;
-        int b = ((int)(Math.random() *100 +1));
         Scanner scan = new Scanner (System.in);
-        System.out.println ("Try to guess a number between 1 and 100. Type that number in.");
-        int a = scan.nextInt();
-       // for (int i = 0; i <= b; i++) {
-        if ((a <1 || a >100)) {
-            System.out.println ("This number is not within the range. Guess again.");
-        }
-        else if (a == i) {
-            System.out.println ("Congratulations, you have guessed correctly! Thank you for playing!!");
-        }
-        else {
-            System.out.println ("This is not the number, try again.");
+        int a = ((int)(Math.random()* 100 +1));
+        boolean b = true;
+        int c = 0;
+        while (b) {
+            System.out.println ("Try to guess a number between 1 and 100. Type that number in.");
+            int x = scan.nextInt();
+            // for (int i = 0; i <= b; i++) {
+            if (x == 0) {
+                break;
+            }
+            else if ((x <1 || x >100)) {
+                System.out.println ("This number is not within the range. Guess again.");
+            }
+            else if (x < a) {
+                System.out.println ("This number is too low, guess again or press 0 to stop the game");;
+                c = c + 1;
+            }
+            else if (x > a) {
+                System.out.println ("This number is too high, guess again or press 0 to stop the game");
+                c = c + 1;
+            }
+            else {
+                System.out.println ("Congratulations, you have guessed the number in " +c+ " tries.");
+                System.out.println ("Would you like to play again? If so, type any number or 0 to quit");
+                int d = scan.nextInt();
+                if (d == 0) {
+                    b = false;
+                }}
         }
     }
 }
-
