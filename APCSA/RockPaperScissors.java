8b7c2c3b4d4280512fe29bf57c4ce73e757bccb7
nicoleadomeit <43471918+nicoleadomeit@users.noreply.github.com>
2018-10-09 08:14:24 -0400

    Update RockPaperScissors.java

--- APCSA/RockPaperScissors.java
+++ APCSA/RockPaperScissors.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 public class RockPaperScissors{
-public static void main(){
-    Scanner scan = new Scanner (System.in);
-    System.out.println ("It is time to play rock, paper, scissors");
-    System.out.println ("Rock, paper, or scissors?");
-}
+    public static void main(){
+        Scanner scan = new Scanner (System.in);
+        System.out.println ("It is time to play rock, paper, scissors");
+        System.out.println ("Please enter your choice of rock, paper, or scissors?");
+        int wins = 0;
+        int loses = 0;
+        int ties = 0;
+        boolean b = true;
+        String r = "rock";
+        String p = "paper";
+        String s = "scissors";
+        r = r.toLowerCase();
+        p = p.toLowerCase();
+        s = s.toLowerCase();
+        
+    }
 }
