62cefa285de5898097d934688e940833b9569499
Kiet Ha <kaitouiet@gmail.com>
2018-10-10 18:09:42 -0400

    there ya go

--- APCSA/RockPaperScissors.java
+++ APCSA/RockPaperScissors.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class RockPaperScissors {
     public static void main(String[] arg) {
-        int i = 0;
         int wins = 0;
         int loses = 0;
         int ties = 0;
@@ -11,59 +10,59 @@ public class RockPaperScissors {
         r = r.toLowerCase();
         p = p.toLowerCase();
         s = s.toLowerCase();
-        String[] object = {"rock", "paper", "scissors"}; 
-        Scanner scan = new Scanner (System.in);
-        System.out.println("You're playing rock, paper, scissors against the computer. Please enter your choice of rock, paper, or scissors, or type exit to quit.");
-        String rps = scan.nextLine();
+        String computerChoice; // using String computerChoice = String.valueOf(computer); is redundant just need to make a string
+        Scanner scan = new Scanner(System.in);
         boolean b = true;
         while (b) {
-            if (rps == "exit") {    
-                b = false;
-                System.out.println ("You have " +wins+ " wins, you have " +loses+ " loses, and you have tied " +ties+ " games.");  
-                break;
-            }
-            double computer = Math.random() * 2 + 1; //random function. generates a number between 0 and 1
-            String computerChoice = String.valueOf(computer);
-            if (computer == 1) {
-                computerChoice = "rock"; 
-            }
-            else if (computer == 2) {
-                computerChoice = "paper"; 
-            }
-            else {
-                computerChoice = "scissors"; 
-            }
-            if (rps == ("exit")) {
-                System.out.println ("You have " +wins+ " wins, you have " +loses+ " loses, and you have tied " +ties+ " games.");      
+            System.out.println("You're playing rock, paper, scissors against the computer. Please enter your choice of rock, paper, or scissors, or type exit to quit."); // u put this outside the while loop, it needs to be in it so it can prompt the user again.
+            String rps = scan.nextLine();
+            int computer = (int) (Math.random() * 2 + 0); //generates an int number from 0 to 2 (makes life easier)
+            if (rps.equals("exit")) {
+                System.out.println("You have " + wins + " wins, you have " + loses + " loses, and you have tied " + ties + " games."); // you had this two times, you only need one if statement for exit. 
                 b = false;
             }
-            if (rps == computerChoice) {
-                System.out.println ("It is a draw!");
+            if (computer == 0) {
+                computerChoice = r;  // if its r then computer choices rock, defined up there
+            } else if (computer == 1) {
+                computerChoice = p;
+            } else {
+                computerChoice = s;
+            }
+            if (rps.equals(computerChoice)) {  // never use == to compare strings. use .equals()
+                System.out.println("It is a draw!");
                 ties = ties + 1;
+
             }
             if (((rps.equals(r) && computerChoice.equals(p)))) {
-                System.out.println ("You lost! Computer chose paper.");
+                System.out.println("You lost! Computer chose paper.");
                 loses = loses + 1;
+
             }
             if (((rps.equals(r) && computerChoice.equals(s)))) {
-                System.out.println ("You won! Computer chose scissors.");
+                System.out.println("You won! Computer chose scissors.");
                 wins = wins + 1;
+
+
             }
             if (((rps.equals(p) && computerChoice.equals(s)))) {
-                System.out.println ("You lost! Computer chose scissors.");
-                loses = loses + 1;  
+                System.out.println("You lost! Computer chose scissors.");
+                loses = loses + 1;
+
             }
             if (((rps.equals(p) && computerChoice.equals(r)))) {
-                System.out.println ("You won! Computer chose rock.");
+                System.out.println("You won! Computer chose rock.");
                 wins = wins + 1;
+
             }
             if (((rps.equals(s) && computerChoice.equals(p)))) {
-                System.out.println ("You won! Computer chose paper.");
+                System.out.println("You won! Computer chose paper.");
                 wins = wins + 1;
+
             }
             if (((rps.equals(s) && computerChoice.equals(r)))) {
-                System.out.println ("You lost! Computer chose rock.");
-                loses = loses + 1;  
+                System.out.println("You lost! Computer chose rock.");
+                loses = loses + 1;
+
             }
         }
     }
